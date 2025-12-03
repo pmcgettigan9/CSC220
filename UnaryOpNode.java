fb49aee920c1094a216a40c774a5a031c634ac67
pmcgettigan9 <PM1027310@wcupa.edu>
2025-12-03 14:10:04 -0500

    Update UnaryOpNode.java

--- UnaryOpNode.java
+++ UnaryOpNode.java
@@ -1,1 +1,16 @@
+public class UnaryOpNode extends Node {
+    public String op;
+    public Node child;
+
+    public UnaryOpNode(String op, Node child) {
+        this.op = op;
+        this.child = child;
+    }
+
+    @Override
+    public void print(String indent) {
+        System.out.println(indent + op);
+        child.print(indent + "  ");
+    }
+}
 
