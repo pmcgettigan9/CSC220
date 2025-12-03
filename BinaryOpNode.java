9fc4fca5783d9197bd94b482750f8f4bf7756a9a
pmcgettigan9 <PM1027310@wcupa.edu>
2025-12-03 14:09:50 -0500

    Update BinaryOpNode.java

--- BinaryOpNode.java
+++ BinaryOpNode.java
@@ -1,1 +1,19 @@
+public class BinaryOpNode extends Node {
+    public String op;
+    public Node left;
+    public Node right;
+
+    public BinaryOpNode(String op, Node left, Node right) {
+        this.op = op;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public void print(String indent) {
+        System.out.println(indent + op);
+        left.print(indent + "  ");
+        right.print(indent + "  ");
+    }
+}
 
