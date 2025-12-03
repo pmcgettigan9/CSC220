182f1243b5666cd13ded7cfc4c9fba347b42c166
pmcgettigan9 <PM1027310@wcupa.edu>
2025-12-03 14:09:35 -0500

    Update NumberNode.java

--- NumberNode.java
+++ NumberNode.java
@@ -1,1 +1,13 @@
+public class NumberNode extends Node {
+    public double value;
+
+    public NumberNode(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public void print(String indent) {
+        System.out.println(indent + value);
+    }
+}
 
