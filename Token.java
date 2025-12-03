45499e5000febea7757f9a04f4e3a6e687d6d48f
pmcgettigan9 <PM1027310@wcupa.edu>
2025-12-03 14:08:51 -0500

    Update Token.java

--- Token.java
+++ Token.java
@@ -1,1 +1,14 @@
+public class Token {
+    public TokenType type;
+    public String value;
+
+    public Token(TokenType t, String v) {
+        this.type = t;
+        this.value = v;
+    }
+
+    public String toString() {
+        return value;
+    }
+}
 
