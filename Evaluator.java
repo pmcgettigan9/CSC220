9754b9f984b799808c70f6ab3e6f9b7c2be9dce7
pmcgettigan9 <PM1027310@wcupa.edu>
2025-12-03 14:10:35 -0500

    Update Evaluator.java

--- Evaluator.java
+++ Evaluator.java
@@ -1,1 +1,30 @@
+public class Evaluator {
+    public double eval(Node node) {
+        if (node instanceof NumberNode) {
+            return ((NumberNode) node).value;
+        }
+
+        if (node instanceof UnaryOpNode) {
+            UnaryOpNode u = (UnaryOpNode) node;
+            if (u.op.equals("-"))
+                return -eval(u.child);
+        }
+
+        if (node instanceof BinaryOpNode) {
+            BinaryOpNode b = (BinaryOpNode) node;
+
+            double left = eval(b.left);
+            double right = eval(b.right);
+
+            switch (b.op) {
+                case "+": return left + right;
+                case "-": return left - right;
+                case "*": return left * right;
+                case "/": return left / right;
+            }
+        }
+
+        throw new RuntimeException("Unknown node type");
+    }
+}
 
